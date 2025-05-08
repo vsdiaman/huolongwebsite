6e3f97c1c11e50b4c1a3c5986cab0634ca7d1f57
Vitor Diamantino <63937106+vsdiaman@users.noreply.github.com>
2025-05-08 16:53:05 -0300

    Update Footer.jsx

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-6">
       <div className="container mx-auto text-center">
-        <p>&copy; ©2025 Huolong Studios, Brazil. All Rights Reserved.</p>
+        <p>&copy;2025 Huolong Studios, Brazil. All Rights Reserved.</p>
         {/* <p>
           <a href="/privacy-policy" className="text-blue-400 hover:underline">
             Privacy Policy
