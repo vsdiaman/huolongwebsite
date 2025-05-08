46abb3bde95ec91b077ecdefb678307011a86d5e
Vitor Diamantino <63937106+vsdiaman@users.noreply.github.com>
2025-05-08 16:47:12 -0300

    Update Footer.jsx
    numero zap

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-6">
       <div className="container mx-auto text-center">
-        <p>&copy; 2025 My Website. All rights reserved.</p>
+        <p>&copy; ©2025 Huolong Studios, Brazil. All Rights Reserved.</p>
         {/* <p>
           <a href="/privacy-policy" className="text-blue-400 hover:underline">
             Privacy Policy
@@ -16,7 +16,7 @@ const Footer = () => {
         </p> */}
         <div className="mt-4">
           <a
-            href="https://www.instagram.com"
+            href="https://www.instagram.com/vitor.diamantino"
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-400 hover:text-pink-500 mx-2"
@@ -24,12 +24,20 @@ const Footer = () => {
             <i className="fab fa-instagram"></i>
           </a>
           <a
-            href="https://www.linkedin.com"
+            href="https://www.linkedin.com/in/vitordiamantino/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-400 hover:text-blue-700 mx-2"
           >
             <i className="fab fa-linkedin"></i>
+          </a>
+          <a
+            href="https://wa.me/5538988645180"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-green-500 hover:text-green-600 mx-2"
+          >
+          <i className="fab fa-whatsapp"></i>
           </a>
         </div>
       </div>
